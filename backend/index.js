6a149c04efb20c58641d2e895dbc9ae7dadb4904
arjun jaiswal <arjunjaiswal4242@gmail.com>
2025-10-25 11:49:43 +0530

    bugs

--- backend/index.js
+++ backend/index.js
@@ -57,6 +57,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Catch all handler: send back React's index.html file for client-side routing
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
