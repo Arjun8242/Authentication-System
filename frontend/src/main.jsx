57a6d66ef682237e514e123750e0bd344d943bf8
arjun jaiswal <arjunjaiswal4242@gmail.com>
2025-10-25 13:06:58 +0530

    added frontendurl

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx';
 import { AppProvider } from './context/AppContext.jsx';
 
- export const server = "http://localhost:5000";
+ export const server = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
